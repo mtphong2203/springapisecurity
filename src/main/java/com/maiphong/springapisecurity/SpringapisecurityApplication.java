af9d3386e50ab934796a91b81dd837492514ed1e
Tien Phong <tienphong2203@gmail.com>
2024-10-17 19:27:22 +0700

    Step 1: Create project spring boot api with security

---
+++ src/main/java/com/maiphong/springapisecurity/SpringapisecurityApplication.java
@@ -0,0 +1,13 @@
+package com.maiphong.springapisecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapisecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapisecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maiphong/springapisecurity/SpringapisecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiphong.springapisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapisecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
